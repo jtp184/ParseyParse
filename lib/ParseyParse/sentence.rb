a795e388647fde737e974cb08f7df5b257e2af1f
Justin Piotroski <justin.piotroski@gmail.com>
2017-08-20 03:30:57 -0700

    Forgot to put the classes in the modules. ++version

--- lib/ParseyParse/sentence.rb
+++ lib/ParseyParse/sentence.rb
@@ -1,24 +1,26 @@
-class Sentence
-  extend Forwardable
+module ParseyParse
+  class Sentence
+    extend Forwardable
 
-  attr_reader :words
+    attr_reader :words
 
-  def_delegators :@words, :first, :last, :length, :each, :map, :find, :find_all, :all?, :any?, :none?, :one?, :select, :reject
+    def_delegators :@words, :first, :last, :length, :each, :map, :find, :find_all, :all?, :any?, :none?, :one?, :select, :reject
 
-  def initialize
-    @words = []
-  end
+    def initialize
+      @words = []
+    end
 
-  def <<(obj)
-    return nil unless obj.is_a? Word
-    @words << obj
-  end
+    def <<(obj)
+      return nil unless obj.is_a? Word
+      @words << obj
+    end
 
-  def to_s
-    words.map(&:to_s).join(' ')
-  end
+    def to_s
+      words.map(&:to_s).join(' ')
+    end
 
-  def root
-    words.find(&:root?)
+    def root
+      words.find(&:root?)
+    end
   end
 end
--- lib/ParseyParse/word.rb
+++ lib/ParseyParse/word.rb
@@ -1,36 +1,38 @@
-class Word
-  def self.field_labels
-    {
-      1 => 'id',
-      2 => 'form',
-      3 => 'lemma',
-      4 => 'pos',
-      5 => 'xpos',
-      6 => 'feats',
-      7 => 'head',
-      8 => 'rel',
-      9 => 'deps',
-      10 => 'misc'
-    }
-  end
-
-  attr_accessor *field_labels.values
-
-  def fields
-    result = {}
-
-    Word.field_labels.each do |_k, v|
-      result[v.to_sym] = method(v.to_sym).call unless method(v.to_sym).call.nil?
+module ParseyParse
+  class Word
+    def self.field_labels
+      {
+        1 => 'id',
+        2 => 'form',
+        3 => 'lemma',
+        4 => 'pos',
+        5 => 'xpos',
+        6 => 'feats',
+        7 => 'head',
+        8 => 'rel',
+        9 => 'deps',
+        10 => 'misc'
+      }
     end
 
-    result
-  end
+    attr_accessor *field_labels.values
 
-  def root?
-    rel == 'ROOT'
-  end
+    def fields
+      result = {}
 
-  def to_s
-    form.to_s
+      Word.field_labels.each do |_k, v|
+        result[v.to_sym] = method(v.to_sym).call unless method(v.to_sym).call.nil?
+      end
+
+      result
+    end
+
+    def root?
+      rel == 'ROOT'
+    end
+
+    def to_s
+      form.to_s
+    end
   end
 end
