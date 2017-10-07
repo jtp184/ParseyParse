859f4427876ecb9585475286942ad5012e501d74
Justin Piotroski <justin.piotroski@gmail.com>
2017-10-06 18:18:16 -0700

    Modified base module to provide the #parse_table and #run_parser methods, and use #call to wrap it nicely for a single string.

--- lib/ParseyParse.rb
+++ lib/ParseyParse.rb
@@ -1,4 +1,5 @@
 require 'ParseyParse/version'
+require 'shellwords'
 
 module ParseyParse
   # The simple Regex pattern to capture each field
@@ -16,11 +17,13 @@ module ParseyParse
     rel
     deps
     misc
-  ].freeze
-end
+    ].freeze
+  end
+  
+  SHELL_COMMAND = "cd %{syntaxnet_path}; echo #{Shellwords.escape(cmd)} | %{script_path} 2>/dev/null" 
 
-require 'ParseyParse/word'
-require 'ParseyParse/sentence'
+  require 'ParseyParse/word'
+  require 'ParseyParse/sentence'
 
 # Takes CoNLL output from Parsey McParseface and converts it into rich featured Ruby Objects for Sentences and component Words/Tokens
 module ParseyParse
@@ -28,7 +31,13 @@ module ParseyParse
   #
   # Takes a string which is the tabular output, splits it by line,
   # and uses that to create each Word, and holds them all in a new Sentence which is then returned
-  def self.call(table_str)
+
+  @@config = {
+    :syntaxnet_path => Dir.home + '/models/syntaxnet',
+    :script_path => 'syntaxnet/pp_generate_table.sh',
+  }
+
+  def self.parse_table(table_str)
     new_sentence = ParseyParse::Sentence.new
 
     table_str.split("\n").each do |line|
@@ -38,11 +47,11 @@ module ParseyParse
 
       scanner.each_with_index do |param, dex|
         vals[FIELD_LABELS[dex]] = case param[0]
-                                  when '_'
-                                    nil
-                                  else
-                                    param[0]
-                                  end
+        when '_'
+          nil
+        else
+          param[0]
+        end
       end
 
       new_word = ParseyParse::Word.new(vals)
@@ -51,5 +60,19 @@ module ParseyParse
     end
 
     new_sentence
+  end
+
+  def self.configure(&blk))
+    yield @@config
+  end
+
+  def self.run_parser(text_str)
+    quoted = "\"#{text_str}\""
+    ParseyParse::SHELL_COMMAND % @@config
+    `#{cmd}`
+  end
+
+  def self.call(str)
+    parse_table(run_parser(str))
   end
 end
